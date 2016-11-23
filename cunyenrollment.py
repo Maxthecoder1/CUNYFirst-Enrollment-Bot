38dd9c15d20e38a35fc71fa523f93c22c722b03e
Max Jeanphilippe <maxjeanphilippe@gmail.com>
2016-11-23 13:33:46 -0500

    Update cunyenrollment.py

--- cunyenrollment.py
+++ cunyenrollment.py
@@ -21,10 +21,10 @@ class CunyFirstEnrollmentShoppingCartNotifier(object):
         self.password =    '??????????'                # CUNYFIRST PASSWORD
         '''In order to receive an email you must go myaccount.google.com/security and scroll all the way to he bottom.
            Then go to where it says 'Allow less secure apps: ON' and check OFF. You should get an email about the change '''
-        self.gmailuser =    '???????????'              # GMAIL USER
-        self.gmailpass =    '??????????'               # GMAIL PASSWORD
-        self.recipient =    '??????????'               # Recipient email address (most likely your email)
-        self.interval = 60                             # refresh interval in seconds
+        self.gmailuser =   '??????????'               # GMAIL USER
+        self.gmailpass =   '??????????'               # GMAIL PASSWORD
+        self.recipient =   '??????????'               # Recipient email address (most likely your email)
+        self.interval = 60                            # refresh interval in seconds
 
         '''Add shopping cart link is very important to this script. Go to CUNYFIRST, Click Enroll. Then choose your Term.
            Once you are are on the Shopping Cart Page, Right + Click on 'add' and Click on 'Copy Link Location'.
